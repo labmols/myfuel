97589e73e48d9880c29c337461bea70facd457ba
labmols <Maor@192.168.1.10>
2015-12-21 14:56:32 +0200

    CarFuel

--- MyFuel/src/myfuel/server/CarFuelDBHandler.java
+++ MyFuel/src/myfuel/server/CarFuelDBHandler.java
@@ -1,7 +1,18 @@
 package myfuel.server;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Observable;
+
+import myfuel.client.Station;
+import myfuel.client.StationInventory;
+import myfuel.response.CarFuelResponse;
+import myfuel.response.Response;
+import myfuel.response.booleanResponse;
 
 public class CarFuelDBHandler extends DBHandler{
 
@@ -10,9 +21,50 @@ public class CarFuelDBHandler extends DBHandler{
 		// TODO Auto-generated constructor stub
 	}
 	
-	private void getInventory()
+	private Response getInventory()
 	{
+		ArrayList <StationInventory> sInventory = new ArrayList<StationInventory>();
+		ArrayList <Float> fQty = new ArrayList<Float>();
+		ArrayList <Float> mQty = new ArrayList<Float>();
+		ArrayList <Station> stations = new ArrayList<Station>();
 		
+		ResultSet rs = null;
+		Statement st = null;
+		PreparedStatement ps = null;
+		String sql;
+		
+		try {
+			st= con.createStatement();
+			sql = "select sid,sname from station";
+			rs = st.executeQuery(sql);
+			while(rs.next()) //Add all stations
+			{
+				stations.add(new Station(rs.getInt(1), rs.getString(2)));
+			}
+				
+			for(Station s : stations)
+			{
+			ps= con.prepareStatement("select fqty,mqty from station_inventory where sid=?");
+			ps.setInt(1, s.getsid());
+			rs= ps.executeQuery();
+			while(rs.next()) // all the sid fuel qty
+			{
+				fQty.add(rs.getFloat(1));
+				mQty.add(rs.getFloat(2));
+				
+			}
+			
+			StationInventory si = new StationInventory(s,fQty,mQty);
+			sInventory.add(si);
+			}
+			
+			return new CarFuelResponse(sInventory);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	@Override
