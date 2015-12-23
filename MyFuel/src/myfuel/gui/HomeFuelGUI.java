51c5d8de3f2a70e46e53d56f17f0a7ada8f3c758
labmols <Maor@192.168.1.13>
2015-12-23 15:15:09 +0200

    homefuel

--- MyFuel/src/myfuel/gui/HomeFuelGUI.java
+++ MyFuel/src/myfuel/gui/HomeFuelGUI.java
@@ -19,14 +19,15 @@ import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import java.awt.GridBagLayout;
 import java.awt.Font;
+import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class HomeFuelGUI extends SuperGUI {
 	
 	HomeFuelActions actions;
-	private JTextField textField;
+	private JTextField qtyText;
 	private JCheckBox urgentCB;
-	private JTextField textField_1;
+	private JTextField shipAddrText;
 	private JDatePickerImpl datePicker;
 	
 	public HomeFuelGUI(HomeFuelActions actions)
@@ -38,26 +39,27 @@ public class HomeFuelGUI extends SuperGUI {
 		lblQuantity.setBounds(141, 176, 115, 16);
 		panel.add(lblQuantity);
 		
-		textField = new JTextField();
-		textField.setBounds(268, 171, 128, 26);
-		panel.add(textField);
-		textField.setColumns(10);
+		qtyText = new JTextField();
+		qtyText.setBounds(268, 171, 128, 26);
+		panel.add(qtyText);
+		qtyText.setColumns(10);
 		
 		urgentCB = new JCheckBox("Urgent Order?");
 		urgentCB.setToolTipText("In 6 hours from now");
 		urgentCB.setHorizontalTextPosition(SwingConstants.LEFT);
 		urgentCB.setHorizontalAlignment(SwingConstants.CENTER);
 		urgentCB.setBounds(268, 252, 128, 23);
+		urgentCB.setOpaque(false);
 		panel.add(urgentCB);
 		
-		JLabel lblNewLabel = new JLabel("Shipping Address:");
-		lblNewLabel.setBounds(141, 216, 134, 16);
-		panel.add(lblNewLabel);
+		JLabel lblShipAddr = new JLabel("Shipping Address:");
+		lblShipAddr.setBounds(141, 216, 134, 16);
+		panel.add(lblShipAddr);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(268, 211, 128, 26);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		shipAddrText = new JTextField();
+		shipAddrText.setBounds(268, 211, 128, 26);
+		panel.add(shipAddrText);
+		shipAddrText.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Is it your correct Address?");
 		lblNewLabel_1.setForeground(Color.WHITE);
@@ -68,9 +70,9 @@ public class HomeFuelGUI extends SuperGUI {
 		lblIfNotEnter.setBounds(401, 236, 174, 16);
 		panel.add(lblIfNotEnter);
 		
-		JLabel lblNewLabel_2 = new JLabel("Ship Date:");
-		lblNewLabel_2.setBounds(141, 296, 76, 29);
-		panel.add(lblNewLabel_2);
+		JLabel lblShipDate = new JLabel("Ship Date:");
+		lblShipDate.setBounds(141, 296, 76, 29);
+		panel.add(lblShipDate);
 		
 		UtilDateModel model1 = new UtilDateModel();
 		Properties p = new Properties();
@@ -95,13 +97,17 @@ public class HomeFuelGUI extends SuperGUI {
 		panel2.setLayout(null);
 		
 		JTextPane txtPane = new JTextPane();
-		txtPane.setFont(new Font("Arial Hebrew", Font.ITALIC, 14));
+		txtPane.setFont(new Font("Arial Hebrew", Font.BOLD | Font.ITALIC, 14));
 		txtPane.setEditable(false);
 		
 		txtPane.setText("Prices:\n\nUrgent Order (in 6 hours from now) : Original price + 2%\n600 - 800 Liters: 3% Discount from Total order price.\nMore then 800 Liters: 4% Discount from Total order price. ");
 		txtPane.setBounds(6, 6, 437, 104);
 		txtPane.setOpaque(false);
 		panel2.add(txtPane);
+		
+		JButton btnNewButton = new JButton("Make Order");
+		btnNewButton.setBounds(255, 371, 117, 29);
+		panel.add(btnNewButton);
 		
 		
 		
