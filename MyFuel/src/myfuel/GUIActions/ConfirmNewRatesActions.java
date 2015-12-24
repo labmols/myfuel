87b9b9bd9f6bcd760fb43f0ded346941634f5651
karmo <karmo@ORON>
2015-12-24 16:29:15 +0200

    c

--- MyFuel/src/myfuel/GUIActions/ConfirmNewRatesActions.java
+++ MyFuel/src/myfuel/GUIActions/ConfirmNewRatesActions.java
@@ -1,75 +1,59 @@
 package myfuel.GUIActions;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 import myfuel.client.MyFuelClient;
+import myfuel.client.saleModel;
 import myfuel.gui.ConfirmNewRatesGUI;
 import myfuel.request.ConfirmNewRatesRequest;
-import myfuel.response.ConfirmRatesResponse;
+import myfuel.request.RequestEnum;
+import myfuel.response.ConfirmNewRatesResponse;
 import myfuel.response.booleanResponse;
-import myfuel.server.ConfirmNewRatesDBHandler;
 
-public class ConfirmNewRatesActions extends GUIActions{
-
-	private ConfirmNewRatesGUI gui ;
-	private ConfirmRatesResponse response;
-	/***
-	 * Create actions class for the ConfirmNewRatesGUI
-	 * @param client - MyFuelClient
-	 */
+public class ConfirmNewRatesActions extends GUIActions {
+	private ConfirmNewRatesGUI gui ; 
+	
 	public ConfirmNewRatesActions(MyFuelClient client) {
 		super(client);
-		gui = new ConfirmNewRatesGUI(this);
-		ConfirmNewRatesRequest request = new ConfirmNewRatesRequest(1);
+		ConfirmNewRatesRequest request = new ConfirmNewRatesRequest(RequestEnum.Select);
 		client.handleMessageFromGUI(request);
+		
+		gui = new ConfirmNewRatesGUI(this);
 		gui.setVisible(true);
 		
 	}
-	/***
-	 * if the company manager wants to confirm the new rates
-	 */
-	public void confirmRates()
-	{
-		ConfirmNewRatesRequest request = new ConfirmNewRatesRequest(2);
-		client.handleMessageFromGUI(request);
-	}
-	
-	/***
-	 * if the company manager wants to deny the new rates
-	 */
-	public void denyRates()
-	{
-		ConfirmNewRatesRequest request = new ConfirmNewRatesRequest(3);
-		client.handleMessageFromGUI(request);
-	}
-	
-	/***
-	 * handling the response from the server
-	 */
 
 	@Override
-	public void update(Observable o, Object arg) {
-		
-		if(arg instanceof ConfirmRatesResponse)
+	public void update(Observable arg0, Object arg1) 
+	{
+		if(arg1 instanceof ConfirmNewRatesResponse)
 		{
-			response =(ConfirmRatesResponse)arg;
-			gui.setLabels(response.getFuels());
+			ArrayList<saleModel> s = (((ConfirmNewRatesResponse)arg1).getsModes());
+			ArrayList<saleModel> c = (((ConfirmNewRatesResponse)arg1).getCurrent());
+			
+			gui.setDetails(s,c);
 		}
 		
-		else if(arg instanceof booleanResponse)
+		else if(arg1 instanceof booleanResponse)
 		{
-			gui.showMessage(((booleanResponse)arg).getMsg());
-			if(((booleanResponse)arg).getMsg().equals("There are no suggested rates"));
-			{
-				changeFrame(gui,new CMActions(client),this);
-			}
+			gui.showMessage(((booleanResponse)arg1).getMsg());
 		}
-		
-	}
-	@Override
-	public void backToMenu() {
 
-					changeFrame(gui,new CMActions(client),this);
+	}
+
+	@Override
+	public void backToMenu() 
+	{
+		changeFrame(gui,new CMActions(client),this);
+	}
+
+	public void sendNewRates(ArrayList<Integer> approved)
+	{
+		if(approved.isEmpty())
+		{
+			
+		}
 		
 	}
 
--- MyFuel/src/myfuel/server/ConfirmNewRatesDBHandler.java
+++ MyFuel/src/myfuel/server/ConfirmNewRatesDBHandler.java
@@ -8,183 +8,80 @@ import java.util.ArrayList;
 import java.util.Observable;
 
 import myfuel.client.Fuel;
+import myfuel.client.saleModel;
 import myfuel.request.ConfirmNewRatesRequest;
-import myfuel.response.ConfirmRatesResponse;
+import myfuel.request.RequestEnum;
+import myfuel.response.ConfirmNewRatesResponse;
 import myfuel.response.booleanResponse;
 
 public class ConfirmNewRatesDBHandler extends DBHandler{
-	
-	private ConfirmNewRatesRequest request;
-	private boolean answer;
-	private ArrayList<Fuel> fuels;
+	private ArrayList<saleModel> sModes;
+	private ArrayList<saleModel> current;
+	private boolean answer ;
 	private String str;
-	
-	/***
-	 *  Create Confirm New Rates  DB Handler
-	 * @param server  - MyFuelServer
-	 * @param con - JDBC driver Connection
-	 */
 	public ConfirmNewRatesDBHandler(MyFuelServer server, Connection con) {
 		super(server, con);
 		
 	}
-	/***
-	 * check if there are any suggested rates in the DB
-	 * if there are it will create an ArrayList<Fuel>
-	 * if not it will set null value for the return response
-	 */
-	void check_suggested_rates()
+
+	private void getDetails()
 	{
 		ResultSet rs = null ;
 		PreparedStatement ps = null;
-	
-		try{
-			ps = con.prepareStatement("select * from suggest_fuel");
-			rs = ps.executeQuery();
-			
-			if(!rs.next())
-			{
-				answer = false;
-				str = "There are no suggested rates";
-			}
-			
-			else
-			{
-				
-				answer = true;
-				get_prices();
-			}
-			
-			
-		}catch(SQLException e){
-			answer = false;
-			str = "There was an error with the server";
-			e.printStackTrace();
-		}
-	}
-	
-	/***
-	 * getting the fuel prices from the DB and put it in the ArrayList<Fuel> fuels
-	 */
-	
-	void get_prices()
-	{
-		ResultSet rs = null ;
-		PreparedStatement ps = null;
-		fuels = new ArrayList<Fuel>();
+		sModes = new ArrayList<saleModel>(); 
+		current = new ArrayList<saleModel>(); 
 		
 		try{
-			ps = con.prepareStatement("SELECT s.price,p.price,p.maxprice "
-										+"FROM fuel_price as p , suggest_fuel as s "
-										+"WHERE p.fuelid = s.fid");
-			rs = ps.executeQuery();
-			while(rs.next())
-			{
-				fuels.add(new Fuel(rs.getFloat(1),rs.getFloat(2),rs.getFloat(3)));
-			}
-			
-		}catch(SQLException e){
-			answer = false;
-			str = "There was an error with the server";
-			e.printStackTrace();
-		}
-		
-	}
-	
-	/***
-	 * Setting the suggested rates as the current ones
-	 * Delete the suggested rates from their table
-	 */
-	void set_new_rates()
-	{
-		ResultSet rs = null ;
-		PreparedStatement ps = null;
-		ArrayList<Float> price = new ArrayList<Float>();
-		
-		try {
-			ps = con.prepareStatement("SELECT p.price FROM suggest_fuel as p");
-			rs = ps.executeQuery();
-			
-			while(rs.next())
-				price.add(rs.getFloat(1));
-			
-			delete_suggested_rates();
-			
-			for(int i=0;i<4;i++)
-			{
-					ps = con.prepareStatement("UPDATE fuel_price SET price = ? WHERE fuelid = ?");
-					
-					ps.setFloat(1, price.get(i));
-					ps.setInt(2,i+1);
-					
-					ps.executeUpdate();
-					
-			}
-			
-			answer = true;
-			str = "New rates has been set successfully";
-		} catch (SQLException e) {
-			answer = false;
-			str = "There was an error with the server";
-			e.printStackTrace();
-		}
-		
-		
-	}
-	
-	/***
-	 * deleting the suggested rates from their table
-	 */
-	void delete_suggested_rates()
-	{
-		PreparedStatement ps = null;
-		
-		try{
-		ps = con.prepareStatement("truncate suggest_fuel");
-		ps.executeUpdate();
-		answer = true ;
-		str = "Suggested rates has been deleted";
-		} catch(SQLException e)
+			   ps = con.prepareStatement("select * from suggest_rates");
+			   
+			   rs = ps.executeQuery();
+			   
+			  while(rs.next())
+			  {
+				  if(rs.getInt(1) != 1)
+					  sModes.add(new saleModel(rs.getInt(1),rs.getInt(2)));
+			  }
+			  
+			  if(sModes.isEmpty())
+			  {
+				  answer = false;
+				  str = "There are no new suggestions";
+				  return;
+			  }
+			  
+			  ps = con.prepareStatement("select * from price_to_type");
+			  rs = ps.executeQuery();
+			  
+			  while(rs.next())
+			  {
+				  if(rs.getInt(1)!= 1)
+					  current.add(new saleModel(rs.getInt(1),rs.getInt(2)));
+			  }
+			  
+			   answer = true;
+			   
+		}catch(SQLException e)
 		{
 			answer = false;
 			str = "There was an error with the server";
 			e.printStackTrace();
 		}
-		
 	}
 	
-	/***
-	 * Handling request from the client
-	 */
 	
 	@Override
 	public void update(Observable o, Object arg) 
 	{
-		
 		if(arg instanceof ConfirmNewRatesRequest)
 		{
-			request = (ConfirmNewRatesRequest)arg;
-			
-			if(request.getType() == 1)
+			ConfirmNewRatesRequest r = (ConfirmNewRatesRequest)arg;
+			if(r.getType() == RequestEnum.Select)
 			{
-				check_suggested_rates();
-				
-				if(answer)
-					server.setResponse(new ConfirmRatesResponse(fuels));
-				else 
+				getDetails();
+				if(!answer)
 					server.setResponse(new booleanResponse(answer,str));
-			}
-			
-			else if(request.getType() == 2)
-			{
-				set_new_rates();
-				server.setResponse(new booleanResponse(answer,str));
-			}
-			
-			else
-			{
-				delete_suggested_rates();
-				server.setResponse(new booleanResponse(answer,str));
+				else
+					server.setResponse(new ConfirmNewRatesResponse(sModes,current));
 			}
 		}
 		
