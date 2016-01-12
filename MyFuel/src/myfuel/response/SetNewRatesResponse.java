6897cff4bfd5eb61090caff04a3a58ec4fc3e9ad
karmo <karmo@ORON>
2016-01-12 15:23:49 +0200

    Set New Rates Updated - Consideration for networks

--- MyFuel/src/myfuel/response/SetNewRatesResponse.java
+++ MyFuel/src/myfuel/response/SetNewRatesResponse.java
@@ -3,25 +3,54 @@ package myfuel.response;
 import java.util.ArrayList;
 
 import myfuel.client.Fuel;
+import myfuel.client.Network;
+import myfuel.client.NetworkRates;
 import myfuel.client.Rate;
 
-
+/***
+ * Response from the server with the Old Rates and Networks Details
+ * @author karmo
+ *
+ */
 @SuppressWarnings("serial")
 public class SetNewRatesResponse extends Response{
-
-	private ArrayList<Rate> oldRates;
 	
-	public SetNewRatesResponse(ArrayList<Rate> oldRates)
+	/***
+	 * Old Rates linked to network
+	 */
+	private ArrayList<NetworkRates> oldRates;
+	/***
+	 * Networks Details
+	 */
+	private ArrayList<Network> networks;
+	
+	/***
+	 * SetNewRatesResponse Constructor
+	 * @param oldRates - Old Rates linked to network
+	 * @param networks - Networks Details
+	 */
+	public SetNewRatesResponse(ArrayList<NetworkRates> oldRates,ArrayList<Network> networks)
 	{
-		this.setoldRates(oldRates);
+		this.setNetworks(networks);
+		this.setOldRates(oldRates);
 	}
 
-	public ArrayList<Rate> getoldRates() {
+	public ArrayList<Network> getNetworks() {
+		return networks;
+	}
+
+	public void setNetworks(ArrayList<Network> networks) {
+		this.networks = networks;
+	}
+
+	public ArrayList<NetworkRates> getOldRates() {
 		return oldRates;
 	}
 
-	public void setoldRates(ArrayList<Rate> oldRates) {
+	public void setOldRates(ArrayList<NetworkRates> oldRates) {
 		this.oldRates = oldRates;
 	}
+
+
 
 }
